634f71c78fed6c53ce22addc877703b2dccc4a27
slevinas <85689866+slevinas@users.noreply.github.com>
2022-05-09 13:20:32 -0400

    Update streamlit_app.py
    adding a multiselect widget

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,5 +12,8 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# adding a selector widget (multy-select. ie' filter)
+streamlit.multiselect('Pick some Fruits:',list(my_fruit_list.index))
 # this will output our df in the app(browser)
 streamlit.dataframe(my_fruit_list)
