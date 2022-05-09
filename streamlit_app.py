a15540219ab5b2e3c0d46f4ca82c1e61f03817c1
slevinas <85689866+slevinas@users.noreply.github.com>
2022-05-09 13:49:16 -0400

    Update streamlit_app.py
    added 2 fruits names to be displayed in the selector widget

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,11 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# set the df index to the fruits names
 my_fruit_list = my_fruit_list.set_index('Fruit')
+
 # adding a selector widget (multy-select. ie' filter)
-streamlit.multiselect('Pick some Fruits:',list(my_fruit_list.index))
+streamlit.multiselect('Pick some Fruits:',list(my_fruit_list.index),['Avocado', 'Strawberries'])
+
 # this will output our df in the app(browser)
 streamlit.dataframe(my_fruit_list)
