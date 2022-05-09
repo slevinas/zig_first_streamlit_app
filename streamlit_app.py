6d3ea7bd7e88aefc5c34aaca6221605474e422f1
slevinas <85689866+slevinas@users.noreply.github.com>
2022-05-09 14:14:26 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,14 @@
-import streamlit
+import streamlit as st
 
-streamlit.title('My Parents New Diner')
+st.title('My Parents New Diner')
 
-streamlit.header('Breakfast Favorites')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
+st.header('Breakfast Favorites')
+st.text('🥣 Omega 3 & Blueberry Oatmeal')
+st.text('🥗 Kale, Spinach & Rocket Smoothie')
+st.text('🐔 Hard-Boiled Free-Range Egg')
+st.text('🥑🍞 Avocado Toast')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
@@ -16,8 +16,26 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # adding a selector widget (multy-select. ie' filter)
-selected_fruits = streamlit.multiselect('Pick some Fruits:',list(my_fruit_list.index),['Avocado', 'Strawberries'])
+selected_fruits = st.multiselect('Pick some Fruits:',list(my_fruit_list.index),['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[selected_fruits]
 
 # this will output our df in the app(browser)
-streamlit.dataframe(fruits_to_show)
+st.dataframe(fruits_to_show)
+
+name = st.text_input('Name')
+if not name:
+  st.warning('Please input a name.')
+  st.stop()
+st.success('Thank you for inputting a name.')
+
+
+
+# #print(my_fruit_list.head(5), end='\n\n')
+# print(my_fruit_list.info() ,end="\n\n")
+# # print(my_fruit_list.index ,end="\n\n")
+# #print(my_fruit_list.loc['Strawberries'] ,end="\n\n")
+
+# # adding a selector widget (multy-select. ie' filter)
+# # st.multiselect('Pick some Fruits:',list(my_fruit_list.index))
+# # # this will output our df in the app(browser)
+# # st.dataframe(my_fruit_list)
