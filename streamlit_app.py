4cf726856fafa429a47d4a82d747c09ef3ab70af
slevinas <85689866+slevinas@users.noreply.github.com>
2022-05-12 10:04:56 -0400

    Update streamlit_app.py
    added a header to the api response and displaying the result as json

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,15 +19,16 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 selected_fruits = st.multiselect('Pick some Fruits:',list(my_fruit_list.index),['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[selected_fruits]
 
+# this will output our df in the app(browser)
+st.dataframe(fruits_to_show)
 
 
 # New Section to display fruityvice api response
 import requests
+st.header('Fruityvice Fruit Advice!')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
 
-# this will output our df in the app(browser)
-st.dataframe(fruits_to_show)
 
 # name = st.text_input('Name')
 # if  name=="":
