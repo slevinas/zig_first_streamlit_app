3c4f5d8911f4e47a617060589a2a14a797effc6c
slevinas <85689866+slevinas@users.noreply.github.com>
2022-05-13 14:42:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,7 +78,7 @@ st.header("The Fruit load list contains:")
 # Adding a button to load the   # function calll... 
 if st.button('Get Fruit Load List'):
   st.write('Thanks for adding ', userinput_fruit_choice2)
-  my_cur.execute("insert into fruit_load_list values (userinput_fruit_choice2)")
+  my_cur.execute("insert into fruit_load_list values (st.text_input('what fruit would you like info about?','tomato'))")
   
 my_data_rows = get_snowflake_fruit_table()
 st.dataframe(my_data_rows)  ##                 Last display a query-result from snowflake's tablez
