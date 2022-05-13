88ccdc27f5a1ede5dc24e5e8f5a0440a62fbe692
slevinas <85689866+slevinas@users.noreply.github.com>
2022-05-13 16:11:17 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,43 +12,33 @@ def get_frutyvice_data(this_fruit_chice):
    # take the json version of the response and normlize it 
   fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
-
-
-  my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-
-
-
-
-
-   
+ 
 st.title('My Parents New Diner')
 st.header('Breakfast Favorites')
 st.text('🥣 Omega 3 & Blueberry Oatmeal')
 st.text('🥗 Kale, Spinach & Rocket Smoothie')
 st.text('🐔 Hard-Boiled Free-Range Egg')
 st.text('🥑🍞 Avocado Toast')
-
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # set the df index to the fruits names
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # adding a selector widget (multy-select. ie' filter)
-selected_fruits = st.multiselect('Pick some Fruits:',list(my_fruit_list.index),['Avocado', 'Strawberries'])
+selected_fruits = st.multiselect('Pick some Fruits:',list(my_fruit_list.index),['Avocado', 'Strawberries']) ## # display output
 fruits_to_show = my_fruit_list.loc[selected_fruits]
 # this will output our df in the app(browser)
-st.dataframe(fruits_to_show) ## 
+st.dataframe(fruits_to_show)                                              ## # display output
   # New Section to display fruityvice api response
-st.header('Fruityvice Fruit Advice!')
-
+st.header('Fruityvice Fruit Advice!')                                     ## # display output               
 try:
-  userinput_fruit_choice = st.text_input('what fruit would you like info about?')
-  if not userinput_fruit_choice:
-    st.error("Please select a fruit to get info. ")
+  fruit_choice = st.text_input('what fruit would you like info about?')   # display text and a grey-text-box-placeholder below the text, to enter input on screen
+
+  if not fruit_choice:
+    st.error("Please select a fruit to get info. ")                       # display red-text-box(errot) and the text("Please select a fruit to get info.)
   else:
-    fruityvice_normalized = get_frutyvice_data(userinput_fruit_choice)
-    st.dataframe(fruityvice_normalized)                           # = output the api-results and display it as a table 
+    fruityvice_normalized = get_frutyvice_data(fruit_choice)
+    st.dataframe(fruityvice_normalized)                           # display output the api-results and display it as a table 
 except URLError as e:
     st.error()
   
@@ -68,8 +58,22 @@ if st.button('Get Fruit Load List'):
   st.dataframe(my_data_rows) ##
   
   
-
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values ('from_streamlit')")
+    return "Thanks for adding " + "new_fruit"
   
+add_my_fruit = st.text_input('what fruit would you like add?')
+if st.button('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+  st.text(insert_row_snowflake(add_my_fruit))
+  
+st.write('Thanks for adding ', add_my_fruit)
+    
+#userinput_fruit_choice =  st.text_input('what fruit would you like info about?')
+#userinput_fruit_choice2 = st.text_input('what fruit would you like info about?','tomato')
+#from_streamlit = userinput_fruit_choice2
+
 
 # don't run anything past het while we trubleshoot
 #st.stop()
@@ -77,22 +81,6 @@ if st.button('Get Fruit Load List'):
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 # st.text("Hello from Snowflake:")
 # st.text(my_data_row)
-
-
-
-
-def insert_row_snowflake(new_fruit):
-  with my_cnx.cursor() as my_cur:
-    my_cur.execute("insert into fruit_load_list values ('from_streamlit')")
-    return "Thanks for adding " + "new_fruit"
-  
-add_my_fruit = st.text_input('what fruit would you like info about?')
-st.write('Thanks for adding ', add_my_fruit)
-    
-#userinput_fruit_choice =  st.text_input('what fruit would you like info about?')
-#userinput_fruit_choice2 = st.text_input('what fruit would you like info about?','tomato')
-#from_streamlit = userinput_fruit_choice2
-
 
 
 
